15b9ae6922ec17adb8763211a3e5257c3dc2f425
2URISTIK <Aleksandr-men-2001@mail.ru>
2023-11-06 19:09:23 +0500

    EnemyAI pt.1
    Create EnemyAI.
    Part 1

---
+++ Swamp-Defender/Assets/EnemyAI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    public float LookRadius;
+    public Animator ani;
+    private void Start()
+    {
+        agent=GetComponent<NavMeshAgent>();
+        target=PlayerManager.instance.player.transform;
+    }
+    private void FixedUpdate()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= LookRadius)
+        {
+            ani.SetBool("IsRun", true);
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                ani.SetBool("IsFight", true);
+                ani.SetBool("IsRun", false);
+                LookTarget();
+            }
+            else
+            {
+                ani.SetBool("IsFight", false);
+                ani.SetBool("IsRun", true);
+            }
+        }
+        else
+        {
+            ani.SetBool("IsRun", false);
+        }
+        
+    }
+    void LookTarget()
+    {
+        Vector3 direction = (target.position-transform.position).normalized;
+        Quaternion look = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime);
+    }
+
+
+}
